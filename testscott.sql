84b05114531a599a5e2b8307eb92b298859e14cf
wodooWindows <insoul_net@qq.com>
2019-04-29 22:11:01 +0800

    完成分页，rownumPL/SQL编程之前

--- testscott.sql
+++ testscott.sql
@@ -6,12 +6,9 @@
 --
 --select nvl(1,0) ,nvl(null,1110) from dual;
 --select nvl2(sal,1,2),nvl2(1,2,3) from dual;
-
 --SELECT * FROM emp;
-
 --select decode(1,1,'丁',2,'美') from dual;
-
---select ename,job,sal, 
+--select ename,job,sal,
 --nvl
 --(
 --decode(
@@ -21,86 +18,169 @@
 --'MANAGER','经理',
 --
 --'PRESIDENT','总裁'
---),'空') 中文工作名 
+--),'空') 中文工作名
 --from emp;
-
 -- 多表查询
-
-select * from emp,dept;
-select e.*,  decode(sg.grade,1,'A',2,'B',3,'C',4,'D',5,'E') 工资等级 from emp e, salgrade sg where e.sal between sg.losal and  sg.hisal; 
-
-select e.ename, e.job,e.sal, d.dname,sg.grade from emp e,dept d,salgrade sg where e.deptno=d.deptno and e.sal between sg.losal and sg.hisal;
-
+SELECT *
+FROM emp,
+  dept;
+SELECT e.*,
+  DECODE(sg.grade,1,'A',2,'B',3,'C',4,'D',5,'E') 工资等级
+FROM emp e,
+  salgrade sg
+WHERE e.sal BETWEEN sg.losal AND sg.hisal;
+SELECT e.ename,
+  e.job,
+  e.sal,
+  d.dname,
+  sg.grade
+FROM emp e,
+  dept d,
+  salgrade sg
+WHERE e.deptno=d.deptno
+AND e.sal BETWEEN sg.losal AND sg.hisal;
 --insert into emp values (9299,'爵爷','coder','7369',sysdate,800,100,null);
-
-select * from emp left join dept on emp.deptno = dept.deptno;
-
-select * from emp e ,dept d where e.deptno=d.deptno(+);
-
-select e1.empno, e1.ename, e1.mgr,e2.ename from emp e1, emp e2 where e1.mgr = e2.empno(+);
-
-select * from emp full outer join dept on emp.deptno=dept.deptno;
-
-select * from emp where empno in (7369,7499);
-
-select deptno 部门编号,count(*) 部门人数 from emp group by deptno order by deptno;
-
-select job,max(sal) 最高工资,min(sal) 最低工资 from emp group by job;
-
-select max((round(avg(sal),2))) g from emp group by deptno ;
-
-select d.dname,count(e.deptno),round(avg(sal)),round(avg((sysdate-e.hiredate)/365)) from emp e left join dept d on e.deptno=d.deptno group by d.dname; 
-
-select s.grade ,count(s.grade),round(avg(e.sal)) from salgrade s , emp e where e.sal between s.losal and s.hisal group by s.grade;
-
-create table myempbak as select * from emp;
-
-create table mydept as select * from dept;
-insert into mydept(deptno,dname,loc) select * from dept;
-
-select rowid, mydept.* from mydept;
-
-create table mydeptgroup as select deptno,dname,loc from mydept group by deptno,dname,loc;
-insert all into mydept values(20,'ACCOUNTING2','NEW YORK') into mydept values(20,'ACCOUNTING','NEW YORK2') into mydept values(20,'ACCOUNTING3','NEW YORK') into mydept values(20,'ACCOUNTING','NEW YORK6') select 1 from dual;
-
-select * from mydept union select * from mydept;
-
-select deptno,dname,loc, count(deptno) from mydept group by deptno,dname,loc;
-
-create table testunion as select * from mydept;
-
-select * from testunion;
-
-update testunion set (deptno,dname,loc)=(select * from mydept where deptno=10);-- 错误的，因为是多行结果，如果改为单行就是正确的
-
+SELECT *
+FROM emp
+LEFT JOIN dept
+ON emp.deptno = dept.deptno;
+SELECT * FROM emp e ,dept d WHERE e.deptno=d.deptno(+);
+SELECT e1.empno,
+  e1.ename,
+  e1.mgr,
+  e2.ename
+FROM emp e1,
+  emp e2
+WHERE e1.mgr = e2.empno(+);
+SELECT * FROM emp FULL OUTER JOIN dept ON emp.deptno=dept.deptno;
+SELECT * FROM emp WHERE empno IN (7369,7499);
+SELECT deptno 部门编号,COUNT(*) 部门人数 FROM emp GROUP BY deptno ORDER BY deptno;
+SELECT job,MAX(sal) 最高工资,MIN(sal) 最低工资 FROM emp GROUP BY job;
+SELECT MAX((ROUND(AVG(sal),2))) g FROM emp GROUP BY deptno ;
+SELECT d.dname,
+  COUNT(e.deptno),
+  ROUND(AVG(sal)),
+  ROUND(AVG((sysdate-e.hiredate)/365))
+FROM emp e
+LEFT JOIN dept d
+ON e.deptno=d.deptno
+GROUP BY d.dname;
+SELECT s.grade ,
+  COUNT(s.grade),
+  ROUND(AVG(e.sal))
+FROM salgrade s ,
+  emp e
+WHERE e.sal BETWEEN s.losal AND s.hisal
+GROUP BY s.grade;
+CREATE TABLE myempbak AS
+SELECT * FROM emp;
+CREATE TABLE mydept AS
+SELECT * FROM dept;
+INSERT INTO mydept
+  (deptno,dname,loc
+  )
+SELECT * FROM dept;
+SELECT rowid, mydept.* FROM mydept;
+CREATE TABLE mydeptgroup AS
+SELECT deptno,dname,loc FROM mydept GROUP BY deptno,dname,loc;
+INSERT ALL
+INTO mydept VALUES
+  (
+    20,
+    'ACCOUNTING2',
+    'NEW YORK'
+  )
+INTO mydept VALUES
+  (
+    20,
+    'ACCOUNTING',
+    'NEW YORK2'
+  )
+INTO mydept VALUES
+  (
+    20,
+    'ACCOUNTING3',
+    'NEW YORK'
+  )
+INTO mydept VALUES
+  (
+    20,
+    'ACCOUNTING',
+    'NEW YORK6'
+  )
+SELECT 1 FROM dual;
+SELECT * FROM mydept
+UNION
+SELECT * FROM mydept;
+SELECT deptno,dname,loc, COUNT(deptno) FROM mydept GROUP BY deptno,dname,loc;
+CREATE TABLE testunion AS
+SELECT * FROM mydept;
+SELECT * FROM testunion;
+UPDATE testunion
+SET
+  (
+    deptno,
+    dname,
+    loc
+  )
+  =
+  (SELECT * FROM mydept WHERE deptno=10
+  );-- 错误的，因为是多行结果，如果改为单行就是正确的
 -- 使用rownum分页 select * from (select rownum rnum, table1.* from table1 where rownum<=(currentpage)*pagsize) temp where temp.rnum>(currentpage-1)*pagesize ;
 -- 疑问：如果我是从2亿数据里面查找第1.9亿之后的十条记录，是不是oracle先查出1.9亿+10条记录，然后再筛选出要求的10条出来，如果是这样，那么内存如何守得住？还是前1.9亿条数据不会一下子查出来，只是游标自动查找到第1.9亿条数据后面的10条？
-select * from (select rownum rn, mydept.* from mydept where rownum<=3*5) temp where temp.rn>(3-1)*5;
-
--- oracle 12c开始使用fetch分页 select * from table offset (currentpage-1)*pagesize rows fetch net pagesize row only;
-select rownum, a.* from myemp a offset 3 rows fetch next 3 row only;
-select rownum, myemp.* from myemp offset (3-1)*3 rows fetch next 3 row only;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT *
+FROM
+  (SELECT rownum rn, mydept.* FROM mydept WHERE rownum<=3*5
+  ) temp
+WHERE temp.rn>(3-1)*5;
+-- oracle 12c开始使用fetch分页 select * from table offset (currentpage-1)*pagesize rows fetch net pagesize row only;注意，下面第一条是从0开始
+SELECT rownum,
+  a.*
+FROM myemp a offset 3 rows
+FETCH NEXT 3 rows only;
+-- 第三页数据
+SELECT rownum, myemp.* FROM myemp offset (3-1)*3 rows
+FETCH NEXT 3 rows only;
+-- row也可以
+SELECT rownum, myemp.* FROM myemp offset (3-1)*3 rows
+FETCH NEXT 3 row only;
+SELECT rownum, myemp.* FROM myemp offset (2-1)*3 rows
+FETCH NEXT 3 rows only;
+-- 测试差集minus用于分页，结果是无法做到的
+(
+SELECT rownum,
+  myemp.*
+FROM myemp
+WHERE rownum <=(3)*3
+)
+INTERSECT
+  (SELECT rownum,myemp.* FROM myemp WHERE rownum>(3-1)*3
+  );
+---------------------------------------- start  PL/SQL编程----------------------------------
+-- 打开服务输出显示
+SET serveroutput ON;
+-- 1，示例
+BEGIN
+  dbms_output.put_line('helloworld');
+END;
+/
+-- 2,变量使用
+DECLARE
+  v_name VARCHAR2(20);
+BEGIN
+  v_name := '韦小宝';
+  dbms_output.put_line('你的名字是：'||v_name);
+END;
+/
+-- 3,键盘输入变量 根据输入的用户编号查找用户名字
+DECLARE
+  v_empno NUMBER(10);
+  v_ename VARCHAR2(20);
+BEGIN
+  v_empno := &eno;
+  SELECT ename INTO v_ename FROM emp WHERE empno=v_empno;
+  dbms_output.put_line('编号 '||v_empno||' 的雇员的名字为 '||v_ename);
+EXCEPTION
+  dbms_output.put_line('没找到数据');
+END;
+/
